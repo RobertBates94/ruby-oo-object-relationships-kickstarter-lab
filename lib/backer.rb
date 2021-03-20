ae6587e5c525364f0fcba68a71858cad18c19bc5
RobertBates94 <bates.to.infinity@gmail.com>
2021-03-20 16:47:31 -0500

    commit tree 0de4efcaeac3e7651c8ff689b776e36eddc61100

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        projects = ProjectBacker.all.select {|project| project.backer == self}
+        projects.collect {|i| i.project}
+    end
+
+end
